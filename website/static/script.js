53a5892ecc7d20f1c032bef7de5f87c4b639a357
lonelyhuskyy <raghavkhanna210@gmail.com>
2023-07-25 14:47:28 +0530

    script final

--- website/static/script.js
+++ website/static/script.js
@@ -9,6 +9,9 @@ selectBtn.addEventListener("click", () => {
     wrapper.classList.toggle("active");
 });
 
+
+
+
 // Function to filter the options based on search input
 function filterOptions(searchText) {
     const options = document.querySelectorAll(".options li");
@@ -20,15 +23,6 @@ function filterOptions(searchText) {
 }
 
 
-const closeButton = document.getElementById('closeBtn');
-const alertDiv = document.getElementById('error')
-
-closeButton.addEventListener('click', () => {
-        // Find the parent alert div and hide it when the close button is clicked
-    alertDiv.style.display = 'none';
-});
-
-
 function selectMovie(movie) {
     const trimmedMovie = movie.trim();
     
@@ -37,6 +31,10 @@ function selectMovie(movie) {
     selectBtn.querySelector("span").textContent = movie; // Update the .select-btn span text here
     wrapper.classList.remove("active");
 }
+
+
+
+
 
 searchInput.addEventListener("input", () => {
     filterOptions(searchInput.value);
@@ -60,6 +58,14 @@ optionsList.addEventListener("click", event => {
     }
 });
 
+
+const closeButton = document.getElementById('closeBtn');
+const alertDiv = document.getElementById('error')
+
+closeButton.addEventListener('click', () => {
+    // Find the parent alert div and hide it when the close button is clicked
+alertDiv.style.display = 'none';
+});
 
 
 
